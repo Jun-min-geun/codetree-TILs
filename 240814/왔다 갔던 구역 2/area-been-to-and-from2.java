507165773bb75ed83348ef62e417450efcae9dcf
JunMinGeun <80248856+Jun-min-geun@users.noreply.github.com>
2024-08-14 18:10:07 +0900

    Title: 왔다 갔던 구역 2, Time: 113ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240814/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] path = new int[1000];
+        int now = 500;
+
+        for (int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+            String direction = sc.next();
+
+            if (direction.equals("R")) {
+                for (int j = 0; j < x; j++) {
+                    path[now]++;
+                    now++;
+                }
+            } else if (direction.equals("L")) {
+                for (int j = 0; j < x; j++) {
+                    path[now]++;
+                    now--;
+                }
+            }
+        }
+
+        int count = 0;
+        for (int i = 0; i < path.length; i++) {
+            if (path[i] >= 2) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
