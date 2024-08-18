1a4db52a75788ca16e0a9e4cf60131436d3ff58d
JunMinGeun <80248856+Jun-min-geun@users.noreply.github.com>
2024-08-18 22:26:32 +0900

    Title: 연속되는 수 2, Time: 122ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240818/연속되는 수 2/continuous-number2.java
+++ 240818/연속되는 수 2/continuous-number2.java
@@ -5,46 +5,38 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n = 0;
+        int n = sc.nextInt();
+        int max = 1;
+
+        int[] numbers = new int[n];
+
+        for(int i = 0; i<n; i++){
+            numbers[i]=sc.nextInt();
+        }
+
         int count = 0;
-
-        while(true){
-             n = sc.nextInt();
-            if(n<1 && n>1000){
-                System.out.println("다시 입력하세요.");
-                continue;
+        for(int i = 1; i<n; i++){
+            if(numbers[i]==numbers[i-1]){
+                count++;
             }else{
-                break;
-            }
-        }
-
-        Integer[] numbers = new Integer[n];
-
-        for(int i = 0; i<n; i++){
-            int j = sc.nextInt();
-            if(0<=j&& j<=1000){
-                numbers[i] = j;
-            }else{
-                System.out.println("똑바로 입력하레이");
-                i--;
-            }
-        }
-
-        for(int i = 0; i<n; i++){
-            int j = 0;
-            for(int a = i; a<n; a++) {
-                if (numbers[i] == numbers[a]) {
-                    j++;
-                }else{
-                    break;
+                if(count>max){
+                    max=count;
                 }
-                if (count < j) {
-                    count = j;
-                }
+                count=1;
             }
+//            for(int a = i; a<n; a++) {
+//                if (numbers[i] == numbers[a]) {
+//                    j++;
+//                }else{
+//                    break;
+//                }
+//                if (count < j) {
+//                    count = j;
+//                }
+//            }
         }
 
-        System.out.println(count);
+        System.out.println(max);
 
 
     }
