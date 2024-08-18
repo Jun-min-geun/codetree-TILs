d296022d5d331a40a75ba703af377f6cda135904
JunMinGeun <80248856+Jun-min-geun@users.noreply.github.com>
2024-08-18 18:03:23 +0900

    Title: 흰검 칠하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240818/흰검 칠하기/painting-white-black.java
+++ 240818/흰검 칠하기/painting-white-black.java
@@ -7,6 +7,8 @@ public class Main {
 
         int n = sc.nextInt();
         int[] array = new int[10000];
+        int[] black = new int[10000];
+        int[] white = new int[10000];
         int now = 5000;
 
         for(int i=0;i<n;i++){
@@ -15,43 +17,33 @@ public class Main {
 
                 if(way.equals("R")){
                     for(int a=0;a<x;a++){
-                        if(array[now]==0){
-                            array[now]=1;
-                        }else if(array[now]==2){
-                            array[now]=4;
-                        } else if (array[now]==3) {
-                            array[now]=5;
-                        }else if(array[now]==6){
-                            array[now]=8;
-                        }
+                        array[now]= 1;
+                        black[now]++;
                         now++;
                     }
+                    now--;
                 }
                 else if(way.equals("L")){
                     for(int b=0;b<x;b++){
-                        if(array[now-1]==0){
-                            array[now-1]=2;
-                        }else if(array[now-1]==1){
-                            array[now-1]=3;
-                        }else if(array[now-1]==4){
-                            array[now-1]=6;
-                        }else if(array[now-1]==5){
-                            array[now-1]=7;
-                        }
+                        array[now]=2;
+                        white[now]++;
                         now--;
                     }
+                    now++;
                 }
         }
-        int countwhite=0;
-        int countblack=0;
+        int countwhite = 0;
+        int countblack = 0;
         int countgray = 0;
 
         for(int j=0; j<10000; j++){
-            if(array[j]==1 || array[j]==4 || array[j]==5){
+            if(array[j]==1 && !(black[j]>=2&&white[j]>=2)){
                 countblack++;
-            }else if(array[j]==2 || array[j]==3|| array[j]==6){
+            }
+            else if(array[j]==2 && !(black[j]>=2&&white[j]>=2)){
                 countwhite++;
-            }else if(array[j]==7||array[j]==8){
+            }
+            else if(black[j]>=2&&white[j]>=2){
                 countgray++;
             }
         }
