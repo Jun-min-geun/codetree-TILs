c20a272365cb8fda308d796c18e11388d1fd7b4e
JunMinGeun <80248856+Jun-min-geun@users.noreply.github.com>
2024-10-16 18:15:33 +0900

    Title: 아름다운 수, Time: 127ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241016/아름다운 수/beautiful-number.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+public class Main {
+    public static int n;
+    public static int count=0;
+    public static ArrayList<Integer> selectedNums = new ArrayList<>();
+
+    // 출력
+    public static boolean checkRight() {
+        for(int i=0;i<n;i++){
+            int num=selectedNums.get(i);
+            if(num==1){
+                continue;
+            }
+            int cnt=1;
+
+            while(i <= n-2) {
+                if(selectedNums.get(i+1) != num)
+                    break;
+                cnt++;
+                i++;
+            }
+            if(cnt % num != 0)
+                return false;
+        }
+
+        return true;
+    }
+
+    public static void findPermutations(int cnt) {
+        // n개 뽑
+        if(cnt == n) {
+            if(checkRight()){
+                count++;
+            }
+            return;
+        }
+
+        //
+        for(int i = 1; i <= 3; i++) {
+            selectedNums.add(i);
+            findPermutations(cnt + 1);
+            selectedNums.remove(selectedNums.size() - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+
+        findPermutations(0);
+
+        System.out.println(count);
+    }
+}
