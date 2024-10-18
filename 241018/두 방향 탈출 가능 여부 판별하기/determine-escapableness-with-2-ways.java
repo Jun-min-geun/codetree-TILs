b757dd2c4909489adab439bccfc6f6a0b81df8b5
JunMinGeun <80248856+Jun-min-geun@users.noreply.github.com>
2024-10-18 23:12:38 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 319ms, Memory: 21MB, Status: Passed - CodeTree

--- 241018/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.java
+++ 241018/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.java
@@ -1,22 +1,35 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
     public static final int MAX_NUM = 100;
+    public static final int DIR_NUM = 2;
+
     public static int n, m;
+    public static int[][] grid = new int[MAX_NUM][MAX_NUM];
+    public static int[][] visited = new int[MAX_NUM][MAX_NUM];
+    public static boolean inRange(int x, int y) {
+        return 0 <= x && x < n && 0 <= y && y < m;
+    }
 
-    public static int[][] graph = new int[MAX_NUM + 2][MAX_NUM + 2];
+    public static boolean canGo(int x, int y) {
+        if(!inRange(x, y))
+            return false;
+        if(visited[x][y] == 1 || grid[x][y] == 0)
+            return false;
+        return true;
+    }
 
-    public static boolean isCertain = false;
-
-    public static void DFS(int startX, int startY) {
-        if(startX==n && startY==m){
-            isCertain=true;
-        }
-        if(graph[startX+1][startY]==1){
-            DFS(startX+1,startY);
-        }
-        if(graph[startX][startY+1]==1){
-            DFS(startX,startY+1);
+    public static void DFS(int x, int y) {
+        int[] dx = new int[]{1, 0};
+        int[] dy = new int[]{0, 1};
+        
+        for(int i = 0; i < DIR_NUM; i++) {
+            int newX = x + dx[i];
+            int newY = y + dy[i];
+            if(canGo(newX, newY)) {
+                visited[newX][newY] = 1;
+                DFS(newX, newY);
+            }
         }
     }
 
@@ -25,18 +38,13 @@ public class Main {
         n = sc.nextInt();
         m = sc.nextInt();
 
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=m;j++){
-                graph[i][j]=sc.nextInt();
-            }
-        }
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < m; j++)
+                grid[i][j] = sc.nextInt();
 
-        DFS(1,1);
+        visited[0][0] = 1;
+        DFS(0, 0);
 
-        if(isCertain){
-            System.out.println(1);
-        }else{
-            System.out.println(0);
-        }
+        System.out.println(visited[n - 1][m - 1]);
     }
 }
