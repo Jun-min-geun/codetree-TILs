351486cadb4d22ba9407cad4300ee6478fbc5561
JunMinGeun <80248856+Jun-min-geun@users.noreply.github.com>
2024-10-24 15:22:50 +0900

    Title: 사각형 채우기 3, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

--- 241024/사각형 채우기 3/rectangle-fill-3.java
+++ 241024/사각형 채우기 3/rectangle-fill-3.java
@@ -1,24 +1,26 @@
 import java.util.Scanner;
 
 public class Main {
-    public static final int MAX_NUM = 1000;
-
-    public static int[] dp = new int[MAX_NUM+1];
-
+    public static final int MOD = 1000000007;
+    public static final int MAX_N = 1000;
+    
     public static int n;
+
+    public static long[] dp = new long[MAX_N + 1];
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         n = sc.nextInt();
 
-        dp[0]=0;
-        dp[1]=2;
-        dp[2]=7;
 
-        for(int i=3; i<=n; i++){
-            dp[i]=(3*dp[i-1]+3*dp[i-2])%1000000007;
+        dp[0] = 1;
+        dp[1] = 2;
+        for(int i = 2; i <= n; i++) {
+            dp[i] = (dp[i - 1] * 2 + dp[i - 2] * 3) % MOD;
+            for(int j = i - 3; j >= 0; j--)
+                dp[i] = (dp[i] + dp[j] * 2) % MOD;
         }
-        System.out.println(dp[n]);
+
+        System.out.print(dp[n]);
     }
 }
